aaffc8c35962a1fc46c9c7c8c613aaf091ac7a6c
Hisan Naeem <hsn37@outlook.com>
2022-03-16 17:32:34 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,38 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-      /* -----------------------------
-       title: 'app routes',
-      // Start the app with the "/" named route. In this case, the app starts
-      // on the FirstScreen widget.
-      initialRoute: '/',
-      routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => const MyHomePage(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        '/Todolist': (context) => TodoList(),
-      },*/
-      
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('To-Do List'),
-        
-
-      )),
-      //home: const MyHomePage(title: 'Ghambeel'),
+      home: const MyHomePage(title: 'Ghambeel'),
     );
   }
 }
