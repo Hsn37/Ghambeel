36f0a3548a7c51cba4f5a2e08d5be41555c187fd
Hisan Naeem <hsn37@outlook.com>
2022-03-22 01:32:40 +0500

    updated the setup function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:ghambeel/modules/login/login.dart';
+import 'package:ghambeel/modules/todolist/todolist.dart';
 import 'modules/storage/storage.dart';
 import 'modules/homepage/homepage.dart';
 import '../../theme.dart';
-// initial varialbes setup.
+
+// initial variables setup.
 void setup() {
   // the flag for whether the user is logged in or not.
   Storage.getValue(Keys.login).then((v) => {
@@ -13,14 +17,26 @@ void setup() {
 
   // the tasks object to be present int the system always
   Storage.getValue(Keys.tasks).then((v) => {
-      if (v == null) 
+      // Priorities: 0 = low, 1 = medium, 2 = high
+      // using empty string if want to overwrite the value
+      if (v == null || v == "") 
         Storage.setValue(Keys.tasks, Storage.jsonEnc({
           "tasks":{
-            "completed":{
-                "task0":{"name":"SampleTask", "priority":"low", "description":"Sample description", "status":"incomplete", "timeAdded":"", "timeCompleted":""}
+            "incomplete":{
+                "task0":{"name":"SampleTask", "priority":"0", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime.now().toString(), "timeCompleted":""},
+                "task2":{"name":"SampleTask2", "priority":"1", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime.now().toString(), "timeCompleted":""},
+                "task3":{"name":"SampleTask2", "priority":"1", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime.now().toString(), "timeCompleted":""},
+                "task4":{"name":"SampleTask2", "priority":"1", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime.now().toString(), "timeCompleted":""},
+                "task5":{"name":"SampleTask2", "priority":"1", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime.now().toString(), "timeCompleted":""},
               },
-            "all":{},
-            "week":{}
+            "complete":{
+                "task1":{"name":"SampleTask", "priority":"2", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime(2021, 2, 2, 2, 2, 2).toString(), "timeCompleted":DateTime.now().toString()},
+                "task6":{"name":"SampleTask", "priority":"2", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime(2021, 2, 2, 2, 2, 2).toString(), "timeCompleted":DateTime.now().toString()},
+                "task7":{"name":"SampleTask", "priority":"2", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime(2021, 2, 2, 2, 2, 2).toString(), "timeCompleted":DateTime.now().toString()},
+                "task8":{"name":"SampleTask", "priority":"2", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime(2021, 2, 2, 2, 2, 2).toString(), "timeCompleted":DateTime.now().toString()},
+                "task9":{"name":"SampleTask", "priority":"2", "description":"Sample description", "status":"incomplete", "timeAdded":DateTime(2021, 2, 2, 2, 2, 2).toString(), "timeCompleted":DateTime.now().toString()}
+            },
+            "week":{"week0":{"date":DateTime.now().toString(), "tasks":["task0"]}}
             }
           }))
     });
@@ -30,13 +46,21 @@ void setup() {
     if (v == null)
       Storage.setValue(Keys.taskNum, 1.toString())
   });
+
+  Storage.getValue(Keys.weekNum).then((v) => {
+    if (v == null)
+      Storage.setValue(Keys.weekNum, 1.toString())
+  });
+
 }
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  // Storage.setValue(Keys.tasks, "").then((p) => setup()); // when you want to flush the tasks object
   setup();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -45,7 +69,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
@@ -53,6 +76,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
