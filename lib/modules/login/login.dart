9a1335a7d8ebc05a334959445399150f0b72a006
DanyalMaq <danyalmaqbool@hotmail.com>
2022-03-21 19:03:27 +0500

    Log in screen styling done

--- lib/modules/login/login.dart
+++ lib/modules/login/login.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:ghambeel/modules/homepage/homepage.dart';
 import 'signup.dart';
-import 'package:mysql1/mysql1.dart';
-import 'package:ghambeel/modules/utils.dart';
+// import 'package:mysql1/mysql1.dart';
+// import 'package:ghambeel/modules/utils.dart';
 
 
 class LoginPage extends StatefulWidget {
@@ -17,17 +18,17 @@ class _LoginPageState extends State<LoginPage> {
   var email = TextEditingController();
   var password = TextEditingController();
 
-  var db = Mysql();
+  //var db = Mysql();
 
-  void getName(name) {
-    db.getConnection().then((connection) {
-      String sql = 'select * from ghambeel.Users;';
-      connection.query(sql).then((results) => {
-        print(results)
-      });
-      connection.close();
-    });
-  }
+  // void getName(name) {
+  //   db.getConnection().then((connection) {
+  //     String sql = 'select * from ghambeel.Users;';
+  //     connection.query(sql).then((results) => {
+  //       print(results)
+  //     });
+  //     connection.close();
+  //   });
+  // }
 
   @override
   void dispose() {
@@ -39,84 +40,106 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text("Login Page"),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.white, Color.fromRGBO(197, 244, 250, 1), Color.fromRGBO(255, 223, 126, 1)]
+        )
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.only(top: 60.0)
-            ),
-            Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                controller: email,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Enter valid email id as abc@gmail.com'),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text("Login Page"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              const Padding(padding: EdgeInsets.only(top: 20)),
+              const Image(
+                image: AssetImage("images/logo.png"),
+                height: 200,
+                width: 200,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 0),
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                controller: password,
-                obscureText: true,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
-              ),
-            ),
-            TextButton(
-              onPressed: (){
-                //TODO FORGOT PASSWORD SCREEN GOES HERE,
-              },
-              child: const Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              child: TextButton(
-                onPressed: () {
-                  //getName("hello");
-                  if (email.text == "danyalmaqbool@hotmail.com" && password.text == "123") {
-                    Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => const MyHomePage(
-                            title: "FLutter")));
-                  }
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
+              const Padding(padding: EdgeInsets.only(top: 20)),
+              Padding(
+                //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  controller: email,
+                  decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.email),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.5),
+                      labelText: 'Email',
+                      hintText: 'Enter valid email id as abc@gmail.com'),
                 ),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const SignupPage()));
-              },
-              child: const Text(
-                "New User? Sign up!"
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 15, bottom: 0),
+                //padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextField(
+                  controller: password,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.password),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.5),
+                      labelText: 'Password',
+                      hintText: 'Enter secure password'),
+                ),
+              ),
+              TextButton(
+                onPressed: (){
+                  //TODO FORGOT PASSWORD SCREEN GOES HERE,
+                },
+                child: const Text(
+                  'Forgot Password',
+                  style: TextStyle(color: Colors.blue, fontSize: 15),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: () {
+                    //getName("hello");
+                    if (email.text == "danyalmaqbool@hotmail.com" && password.text == "123") {
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => const MyHomePage(
+                              title: "FLutter")));
+                    }
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 20)),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const SignupPage()));
+                  },
+                  child: const Text(
+                      "New User? Sign up!"
+                  )
               )
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
 
 class SecondRoute extends StatelessWidget {
   const SecondRoute({Key? key}) : super(key: key);
