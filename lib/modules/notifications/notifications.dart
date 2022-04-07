21b98182a7ba6a23235eca2a361bcc031af6a7e8
Hisan Naeem <hsn37@outlook.com>
2022-04-08 00:23:05 +0500

    set up the notifications module

---
+++ lib/modules/notifications/notifications.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+class Notifications {
+
+  static Future init() async {
+    final AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('appicon');
+    
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid, 
+            iOS: null, 
+            macOS: null);
+
+    return flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  static void onSelectNotification(String? payload) async {
+    // Function where we handle the notification clicks. check the payload.
+    print("Clicked on notification");
+  }
+
+  static Future<void> show(String title, String content, NotifID id) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails('ghambeel1', 'ghambeelNotif',
+            channelDescription: 'ghambeelNotif',
+            importance: Importance.max,
+            priority: Priority.max,
+            ticker: 'ticker',
+            playSound: true);
+    
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    return await flutterLocalNotificationsPlugin.show(
+        id.index, title, content, platformChannelSpecifics,
+        payload: 'item x'
+      );
+  }
+}
+
+enum NotifID {
+  motquote,
+  deadline,
+}
